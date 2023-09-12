e6392562fb02657b0fe5efacbd48145fb2c55387
Elwyn Fernandes <elwynfernandes@Elwyns-Macbook.local>
2023-09-12 14:50:43 +0100

    remove gitignore files

--- database/database.js
+++ database/database.js
@@ -1,26 +1,30 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 module.exports = () => {
+  //new databse connected
+  db = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    multipleStatements: false, //multiple statements allow for SQL injections
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 
-    //new databse connected
-    db = new Pool({
-        user: process.env.PGUSER,
-        host: process.env.PGHOST,
-        database: process.env.PGDATABASE,
-        password: process.env.PGPASSWORD,
-        port: process.env.PGPORT,
-        multipleStatements: false //multiple statements allow for SQL injections
-    });
+  db.connect(function (error) {
+    if (!!error) {
+      console.log(error);
+    } else {
+      console.log("Connected!:)");
+    }
+  });
 
-
-    db.connect(function(error){
-        if(!!error){
-            console.log(error);
-        }else{
-            console.log('Connected!:)');
-        }
-    });
-
-    return db;
-
-}
+  return db;
+};
